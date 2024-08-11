81135abdf1bfeb2c1878285ae6bf0fe1a8343c47
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-08-11 11:45:05 +0300

    Added a parseFloat to the amount in formData, Created a  return statement

--- src/Form.js
+++ src/Form.js
@@ -8,17 +8,20 @@ function Form() { // State variables
 
     const handleOnChange = (set) => (e)=> set(e.target.value);
 
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
     const formData = {
         date,
         description,
         category,
-        amount
+        amount: parseFloat(amount)
     };
 
     // Fetch request
     fetch("https://bank-of-flatiron-backend-three.vercel.app/transactions", {
         method: "POST",
-        header: {
+        headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
         },
@@ -38,5 +41,59 @@ function Form() { // State variables
     setCategory('');
     setAmount('');
 };
+
+return (
+    <div className="form">
+        <form className="form-blocks" onSubmit={handleSubmitForm}>
+            
+            <div>
+                <h1 className="transaction-form">
+                    Transaction Form
+                </h1>
+            </div>
+
+            <input
+                className="form-input"
+                type="date"
+                placeholder="Date"
+                value={date}
+                onChange={handleOnChange(setDate)}
+            />
+
+            <input
+                className="form-input"
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={handleOnChange(setDescription)}
+            />
+
+            <input
+                className="form-input"
+                type="text"
+                placeholder="Category"
+                value={category}
+                onChange={handleOnChange(setCategory)}
+            />
+
+            <input
+                className="form-input"
+                type="number"
+                placeholder="Amount"
+                value={amount}
+                onChange={handleOnChange(setAmount)}
+            />
+
+            <input
+            className="submit"
+            type="submit"
+            value="Add Transaction"
+            />
+
+        </form>        
+    </div>
+);
+
+}
 
 export default Form;
