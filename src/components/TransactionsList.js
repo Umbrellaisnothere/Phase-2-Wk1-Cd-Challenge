0c3a69bd80d25919569cb66664198e3e4b254ea3
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-08-11 13:51:52 +0300

    Created a function and fetch to dispaly the list of transactions

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -3,7 +3,18 @@ import React, { useEffect, useState } from "react";
 // import Transaction from "./Transaction";
 
 function TransactionsList() {
+    const [transactions, setTransactions] = useState([]);
+    const [filteredTransactions, setFilteredTransactions] = useState([]);
 
+    useEffect(() => {
+        fetch("https://bank-of-flatiron-backend-three.vercel.app/transactions")
+            .then((response) => response.json())
+            .then((data) => {
+                setTransactions(data);
+                setFilteredTransactions(data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
     
     return (
         <div>Transactions List</div>
