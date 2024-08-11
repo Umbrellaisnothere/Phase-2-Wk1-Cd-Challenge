5192b83c4eb65c686d984a3d9cbc46c0eeec2f35
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-08-11 15:14:01 +0300

    Made a return statement for the TransactionList.js Created a delete button in Transaction.js

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-// import FilterTransactions from "./FilterTransactions";
-// import Transaction from "./Transaction";
+import FilterTransactions from "./FilterTransactions";
+import Transactions from "./Transaction";
 
 function TransactionsList() {
     const [transactions, setTransactions] = useState([]);
@@ -15,10 +15,42 @@ function TransactionsList() {
             })
             .catch((error) => console.log(error));
     }, []);
+
+    const handleDelete = (id) => {
+        console.log("Delete transaction with id:", id);
+        setFilteredTransactions(filteredTransactions.filter(transtion => transtion.id !== id));
+    };
     
     return (
-        <div>Transactions List</div>
-    );
+            <div>
+
+              <h1 className="Transaction-form">
+                Transaction List
+              </h1>
+
+              <FilterTransactions transactions={transactions} onFilter={setFilteredTransactions} />
+              
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {filteredTransactions.map((transtion) => (
+                    <Transactions key={transtion.id} transtion={transtion} onDelete={handleDelete} />
+                  ))}
+                </tbody>
+                
+              </table>
+
+            </div>
+          );
 }
 
 
